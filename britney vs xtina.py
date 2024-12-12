fa6240263a047687b8bc9c12bdaae73e57606d47
KennedyMason <kennedy.mason@ucas-edu.net>
2024-12-11 18:31:23 -0700

    Add files via upload

---
+++ britney vs xtina.py
@@ -0,0 +1,7 @@
+import pygame
+
+#set screen size and caption
+WIN = pygame.display.set_mode(900,500)
+pygame.display.set_caption("britney spears and christina aguilera fight to the death")
+
+BRITNEY = pygame.image.load()
